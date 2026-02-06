71a3e1f892b7a839663901f7f2dfed4785282fa1
drescher-christoph <chriscode03@gmail.com>
2026-02-06 14:22:53 +0100

    finish swap method and swap python simulation. next step: building the router contract

--- src/InvariantPair.sol
+++ src/InvariantPair.sol
@@ -13,6 +13,15 @@ contract InvariantPair {
 
     bool private initialized;
 
+    event Swap(
+        address indexed sender,
+        uint256 amount0In,
+        uint256 amount1In,
+        uint256 amount0Out,
+        uint256 amount1Out,
+        address indexed to
+    );
+
     constructor() {
         factory = msg.sender;
     }
@@ -61,13 +70,29 @@ contract InvariantPair {
 
         // Optimistically transfer wanted output
         if (amount0Out > 0) _transfer(token0, to, amount0Out);
-        if (amount0Out > 1) _transfer(token1, to, amount1Out);
+        if (amount1Out > 0) _transfer(token1, to, amount1Out);
 
         uint256 _balance0 = IERC20(token0).balanceOf(address(this));
         uint256 _balance1 = IERC20(token1).balanceOf(address(this));
 
-        uint256 amount0In =
+        // dx = (dy * x0) / (y0 - dy)
+        // dx...expected input; dy...amountOut; x0...reserve of input token; y0...reserve of output token
+        uint256 amount0InExpected = (amount0Out * reserve0) / (reserve1 - amount0Out);
+        uint256 amount1InExpected = (amount1Out * reserve1) / (reserve0 - amount1Out);
+        require(amount0InExpected > 0 || amount1InExpected > 0, "InvariantAMM: INSUFFICIENT_INPUT_ERROR");
+
+        // now we have to check if amounIn is including the fees: balance * 1000 - input * 3 > reserve0 * reserve1 * 1000^2 
+        uint256 balance0Adjusted = (_balance0 * 1000) - (amount0InExpected * 3);
+        uint256 balance1Adjusted = (_balance1 * 1000) - (amount1InExpected * 3);
+        require(balance0Adjusted * balance1Adjusted >= reserve0 * reserve1 * (1000**2), "InvariantAMM: K_INVARIANT_ERROR");
+
+        _update(_balance0, _balance1);
+        emit Swap(msg.sender, amount0InExpected, amount1InExpected, amount0Out, amount1Out, to);
+
     }
 
-    function _update(uint256 balance0, uint256 balance1) internal {}
+    function _update(uint256 balance0, uint256 balance1) internal {
+        reserve0 = balance0;
+        reserve1 = balance1;
+    }
 }
